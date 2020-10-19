27b566f2c9006fd3e8483bf5f5b9e8571ca05032
SimonBesendorfer <simon.besendorfer@me.com>
2020-10-19 10:24:46 +0200

    added kakteen

--- game.js
+++ game.js
@@ -55,8 +55,7 @@ function updateCaracter() {
 }
 
 function drawGround(){
-    ctx.fillStyle = "#FFE699";
-    ctx.fillRect(0, 375, canvas.width, canvas.height - 375);
+    
 
 
     if(isMovingRight){
@@ -66,18 +65,28 @@ function drawGround(){
     if(isMovingLeft){
         bg_elements = bg_elements +2;
     }
+    addBackgroundObject('img/bg_elem_1.png', 0, 195, 0.6, 0.4);
+    addBackgroundObject('img/bg_elem_2.png', 450, 120, 0.6, 0.5);
+    addBackgroundObject('img/bg_elem_1.png', 700, 255, 0.4, 0.7);
+    addBackgroundObject('img/bg_elem_2.png', 1200, 260, 0.3), 0.5;
 
-    let base_image = new Image();
-    base_image.src = 'img/bg_elem_1.png';
-    if (base_image.complete) {
-        ctx.drawImage(base_image, bg_elements, 195, base_image.width * 0.6, base_image.height * 0.6);
+
+    //draw Ground
+    ctx.fillStyle = "#FFE699";
+    ctx.fillRect(0, 375, canvas.width, canvas.height - 375);
+
+}
+
+function addBackgroundObject(src, offsetX, offsetY, scale, opacity){
+    if (opacity != undefined){
+        ctx.globalAlpha = opacity;
     }
-
     let base_image2 = new Image();
-    base_image2.src = 'img/bg_elem_2.png';
+    base_image2.src = src;
     if (base_image2.complete) {
-        ctx.drawImage(base_image2, 450 + bg_elements, 120, base_image2.width * 0.6, base_image2.height * 0.6);
+        ctx.drawImage(base_image2, offsetX + bg_elements, offsetY, base_image2.width * scale, base_image2.height * scale);
     }
+    ctx.globalAlpha = 1;
 }
 
 function listenForKeys(){
@@ -115,4 +124,4 @@ function listenForKeys(){
     });
 }
 
-//last watched Video 09 - Fall implementieren
+//last watched Video 12 - Weitere Kakteen einfügen
