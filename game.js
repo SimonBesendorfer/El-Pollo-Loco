04760fc72f0bb74762947e23ab62bdc5f3ba47ed
SimonBesendorfer <simon.besendorfer@me.com>
2020-10-23 14:42:29 +0200

    not finished

--- game.js
+++ game.js
@@ -358,10 +358,10 @@ function addBackgroundObject(src, offsetX, offsetY, scale, opacity){
     if (opacity != undefined){
         ctx.globalAlpha = opacity;
     }
-    let base_image2 = new Image();
-    base_image2.src = src;
-    if (base_image2.complete) {
-        ctx.drawImage(base_image2, offsetX + bg_elements, offsetY, base_image2.width * scale, base_image2.height * scale);
+    let base_image = new Image();
+    base_image.src = src;
+    if (base_image.complete) {
+        ctx.drawImage(base_image, offsetX + bg_elements, offsetY, base_image.width * scale, base_image.height * scale);
     }
     ctx.globalAlpha = 1;
 }
@@ -412,4 +412,62 @@ function listenForKeys(){
     });
 }
 
-//last watched Video 29 - Endgegner einfügen
+/**
+
+* Preload all images. This function should be executed before starting the game.
+
+* imagePaths should contain all images that will be loaded: ['img/image1.png', 'img/image2.png', 'img/image3.png', ...]
+
+*/
+
+function preloadImages() {
+
+    for (let i = 0; i < imagePaths.length; i++) {
+  
+      let image = new Image();
+  
+      image.src = imagePaths[i];
+  
+      images.push(image); // push image-path to images-array (which contains all image-paths)
+  
+    }
+  
+  }
+  
+  
+  
+  
+  
+  /**
+  
+     * Check if background-image is already loaded in cache; if not, create new image
+  
+     * @param {string} src_path - scr-path of background-image 
+  
+     */
+  
+  function checkBackgroundImageCache(src_path) {
+  
+    // Check if image is found in images-array.
+  
+    base_image = images.find(function (img) {
+  
+      return img.src.endsWith(src_path.substring(src_path, src_path.length));
+  
+    })
+  
+    
+  
+    // Create new image if not found in cache
+  
+    if (!base_image) {
+  
+      base_image = new Image();
+  
+      base_image.src = src_path;
+  
+    }
+  
+  }
+  
+  
